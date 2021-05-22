2a948c8e69b74e03d8be16d7011a3e4b1fabdfa9
divyanshi-goyal <divyanshigoyal.met18@itbhu.ac.in>
2021-05-22 19:25:34 +0530

    Final project

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://divyanshi-memories-project.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://inter-active.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
