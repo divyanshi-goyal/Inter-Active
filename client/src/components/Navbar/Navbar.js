485e6c1c9e07765602d9f23b21244149002bc0f9
divyanshi-goyal <divyanshigoyal.met18@itbhu.ac.in>
2021-05-22 16:41:08 +0530

    token changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React , { useState, useEffect} from 'react';
 import useStyles from './styles';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 import { AppBar, Toolbar, Typography, Avatar, Button  } from '@material-ui/core'; 
 import memories from '../../images/memory.png';
 
@@ -22,6 +23,12 @@ const Navbar = () => {
    
     useEffect(() => {
         const token = user?.token;
+
+        if(token){
+            const decodedToken = decode(token);
+
+            if(decodedToken.exp * 1000 <new Date().getTime()) logout();
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location])
